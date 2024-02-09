10b5bcfd9084a7e14858ac9ee55410038530c40d
Torremolinos <adrian_2054@hotmail.com>
2024-02-09 12:18:13 +0100

    Estructura php

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -10,10 +10,12 @@
                 <p><span class="glyphicon glyphicon-envelope"></span> pelayinBollu@hotmail.com</p>
             </div>
     </footer>
+
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
     <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
     <script src='//cdnjs.cloudflare.com/ajax/libs/FitText.js/1.1/jquery.fittext.min.js'></script>
     <script src="/scripts/main.js"></script>
+    
 </body>
 
 </html>
