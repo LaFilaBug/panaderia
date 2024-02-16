1fb8a38049022092b023d6ccaaed080fce8b3a85
Laura López <99351746+laurity@users.noreply.github.com>
2024-02-16 10:05:39 +0100

    añadido header y footer a todo

--- contacto.php
+++ contacto.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Contacto</title>        
-</head>
-<body>
+<?php
+include 'includes/templates/header.php';
+?>
     <section id="contacto" class="contacto2">
         <h1>Telefono de contacto</h1>
         <p>+34 948 69 23 80</p>
@@ -19,5 +12,6 @@
         <a href="/index.php">Volver a la página principal</a>
     </section>
     <script src="/scripts/index.js"></script>
-</body>
-</html>
+    <?php
+include 'includes/templates/footer.php';
+?>
