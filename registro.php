f4c1e18e52a871a2b2678d9c935c49cde1d204ed
Rocket201 <alejandrocorroto99@gmail.com>
2024-02-20 13:04:45 +0100

    registr

--- registro.php
+++ registro.php
@@ -22,8 +22,8 @@
     this.classList.add('moveUp'); // Agrega la clase 'moveUp' al enlace
     setTimeout(function() {
         window.location.href = 'inicioSesion.php'; // Redirige a la página principal
-    }, 1000); // Espera 1 segundo (la duración de la animación) antes de redirigir
-});
+            }, 1000); // Espera 1 segundo (la duración de la animación) antes de redirigir
+        });
 </script>
 </body>
 </html>
