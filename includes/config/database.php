371970064db055535cecb3462d518e5a2dfd46ea
Laura López <99351746+laurity@users.noreply.github.com>
2024-02-20 12:51:14 +0100

    correcion bbdd

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,28 +1,13 @@
-<?php
-function conectarBD()
+<?php 
 
-{
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost', 'root', '', 'lafilabug');
 
-    $cadena_conexion = 'mysql:dbname=dwes_t3;host=127.0.0.1';
-
-    $usuario = "root";
-
-    $clave = "";
- 
-    try {
-
-        $bd = new PDO($cadena_conexion, $usuario, $clave);
-
-        return $bd;
-
-    } catch (PDOException $e) {
-
-        echo "Error conectar BD: " . $e->getMessage();
-
+    if(!$db) {
+        echo "Error no se pudo conectar";
         exit;
+    } 
 
-    }
-
+    return $db;
+    
 }
-
-$conn = conectarBD();
