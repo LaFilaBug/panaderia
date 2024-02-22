6644f5897007b074ebe6fa40b6173338a6cd9feb
Laura Lopez <lauralopezalonsodev@gmail.com>
2024-02-22 17:04:18 +0100

    subida

--- registro.php
+++ registro.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/stylesRegistro.css">
-    <title>Registro</title>
+    <title>Registro del cliente</title>
 </head>
 
 <body>
