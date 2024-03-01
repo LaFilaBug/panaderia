b648db81044aad1dc789afd299f6cf09fe3c1389
Torremolinos <adrian_2054@hotmail.com>
2024-03-01 10:05:21 +0100

    añado el js del darkmode el boton y los estilos y tengo que hacerlo funcionar

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -20,6 +20,10 @@ if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
     <meta name="viewport" content="width=device-width" />
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
     <link rel="stylesheet" href="styles/main.css"/>
+    <style>
+
+
+    </style>
 </head>
 
 <body>
@@ -56,6 +60,7 @@ if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
                     <?php if (isset($_SESSION['usuario'])) : ?>
                         <li><a href="index.php?logout=true">Cerrar sesión</a></li>
                     <?php endif; ?>
+                    <li><a><button class="dark">🌙</button></a></li>
                 </ul>
             </div>
         </div>
