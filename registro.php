ce1a1bb0e095dffbe4b40ac619dfa83d375af10c
Laura López <99351746+laurity@users.noreply.github.com>
2024-03-05 13:38:33 +0100

    migrado sass registro

--- registro.php
+++ registro.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/stylesRegistro.css">
+    <link rel="stylesheet" href="styles/main.css">
     <title>Registro del cliente</title>
 </head>
 
