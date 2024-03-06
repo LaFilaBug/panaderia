2013dce4b51a39a9f7698080cb6af6db0a73f4d4
Torremolinos <adrian_2054@hotmail.com>
2024-03-06 18:25:11 +0100

    Zona Admin estilos y media Query Acabado.

--- admin/index.php
+++ admin/index.php
@@ -101,6 +101,8 @@ function listarPanaderia()
 
 <head>
     <link rel="stylesheet" type="text/css" href="./../styles/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
     <title>Panadería</title>
 </head>
 
