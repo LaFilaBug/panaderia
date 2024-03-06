214583c1db93ec385b2a14698946e533f898d2ce
Laura Lopez <lauralopezalonsodev@gmail.com>
2024-03-06 15:24:12 +0100

    asdasd

--- pagina_destino.php
+++ pagina_destino.php
@@ -12,7 +12,6 @@ include 'includes/templates/header.php';
 
 $bd = conectarDB();
 
-// Asegúrate de que el ID del producto está disponible, por ejemplo, a través de GET o POST
 if (isset($_GET['id'])) {
     $idProducto = $_GET['id'];
 
@@ -23,7 +22,6 @@ if (isset($_GET['id'])) {
     $resultado = $consulta->get_result();
     $producto = $resultado->fetch_assoc();
     if ($producto) {
-        // Muestra el producto
         ?>
         <body>
             <div class="producto-destino">
